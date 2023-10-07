d2ce7f1a40bc56092ad73c8cc4e44f5f57646d0f
Alexander <sacha05bugaev@gmail.com>
2023-10-07 13:27:09 +0300

    добавить тесты

--- week4/phoneBook/phoneBook.c
+++ week4/phoneBook/phoneBook.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 
 #define NAME_OF_FILE "phoneBook.txt"
+#define TEST_FILE_1 "test1.txt"
+#define TEST_FILE_2 "test2.txt"
+#define TEST_FILE_3 "test3.txt"
 
 typedef struct {
     char* name;
@@ -14,6 +17,33 @@ typedef struct {
     Note* notes;
     int numberOfNotes;
 } Phonebook;
+
+int addNoteSupport(Phonebook* phonebook, const char* name, const char* number)
+{
+    Note note = { "", "" };
+    int size = sizeof(char) * 64;
+    note.name = malloc(size);
+    note.number = malloc(size);
+    if (note.name == NULL || note.number == NULL)
+    {
+        free(note.name);
+        free(note.number);
+        return 1;
+    }
+    int errorCode = strcpy_s(note.name, size, name);
+    errorCode += strcpy_s(note.number, size, number);
+    if (errorCode != 0)
+    {
+        free(note.name);
+        free(note.number);
+        return 1;
+    }
+    int* numberOfNotes = &phonebook->numberOfNotes;
+    int index = *numberOfNotes;
+    phonebook->notes[index] = note;
+    ++* numberOfNotes;
+    return 0;
+}
 
 int readNotesFromFile(Phonebook* phonebook, const char* nameOfFile)
 {
@@ -35,35 +65,23 @@ int readNotesFromFile(Phonebook* phonebook, const char* nameOfFile)
     int* numberOfNotes = &phonebook->numberOfNotes;
     while (!feof(inputFile) && *numberOfNotes < 100)
     {
-        Note note = { "", "" };
         char name[64] = { '\0' };
         fscanf_s(inputFile, "%[^ ]", name, 63);
-        fseek(inputFile, 1, SEEK_CUR);
         if (strlen(name) == 0)
         {
             break;
         }
+        fseek(inputFile, 1, SEEK_CUR);
         char number[64] = { '\0' };
         fscanf_s(inputFile, "%[^\n]", number, 63);
         fseek(inputFile, 2, SEEK_CUR);
 
-        int size = sizeof(char) * 64;
-        note.name = malloc(size);
-        note.number = malloc(size);
-        if (note.name == NULL || note.number == NULL)
-        {
-            fclose(inputFile);
-            return 1;
-        }
-        errorCode = strcpy_s(note.name, size, name);
-        errorCode += strcpy_s(note.number, size, number);
+        errorCode = addNoteSupport(phonebook, name, number);
         if (errorCode != 0)
         {
             fclose(inputFile);
             return 1;
         }
-        phonebook->notes[*numberOfNotes] = note;
-        ++* numberOfNotes;
     }
     fclose(inputFile);
     return 0;
@@ -77,7 +95,6 @@ int addNote(Phonebook* phonebook)
         printf("Cannot add any more notes\n");
         return 0;
     }
-    Note note = { "", "" };
     char name[64] = { '\0' };
     printf("Enter name: ");
     scanf_s("%s", name, 63);
@@ -85,22 +102,11 @@ int addNote(Phonebook* phonebook)
     printf("Enter number: ");
     scanf_s("%s", number, 63);
 
-    int size = sizeof(char) * 64;
-    note.name = malloc(size);
-    note.number = malloc(size);
-    if (note.name == NULL || note.number == NULL)
-    {
-        return 1;
-    }
-    int errorCode = strcpy_s(note.name, size, name);
-    errorCode += strcpy_s(note.number, size, number);
+    int errorCode = addNoteSupport(phonebook, name, number);
     if (errorCode != 0)
     {
         return 1;
     }
-    int index = *numberOfNotes;
-    phonebook->notes[index] = note;
-    ++* numberOfNotes;
     printf("Note added\n");
     return 0;
 }
@@ -119,7 +125,7 @@ void printAllNotes(const Phonebook* phonebook)
     }
 }
 
-bool stringsAreEqual(char* string1, char* string2)
+bool stringsAreEqual(const char* string1, const char* string2)
 {
     size_t lengthOfString1 = strlen(string1);
     size_t lengthOfString2 = strlen(string2);
@@ -137,11 +143,8 @@ bool stringsAreEqual(char* string1, char* string2)
     return true;
 }
 
-void findNumberByName(const Phonebook* phonebook)
+char* findNumberByNameSupport(const Phonebook* phonebook, const char* nameToFind)
 {
-    char nameToFind[64] = { '\0' };
-    printf("Enter a name: ");
-    scanf_s("%s", nameToFind, 63);
     Note* notes = phonebook->notes;
     int numberOfNotes = phonebook->numberOfNotes;
     for (int i = 0; i < numberOfNotes; ++i)
@@ -149,18 +152,28 @@ void findNumberByName(const Phonebook* phonebook)
         Note note = notes[i];
         if (stringsAreEqual(note.name, nameToFind))
         {
-            printf("%s\n", note.number);
-            return;
+            return note.number;
         }
     }
-    printf("Name not found\n");
+    return NULL;
 }
 
-void findNameByNumber(const Phonebook* phonebook)
+void findNumberByName(const Phonebook* phonebook)
 {
-    char numberToFind[64] = { '\0' };
-    printf("Enter a number: ");
-    scanf_s("%s", numberToFind, 63);
+    char nameToFind[64] = { '\0' };
+    printf("Enter a name: ");
+    scanf_s("%s", nameToFind, 63);
+    char* foundNumber = findNumberByNameSupport(phonebook, nameToFind);
+    if (foundNumber == NULL)
+    {
+        printf("Name not found\n");
+        return;
+    }
+    printf("%s\n", foundNumber);
+}
+
+char* findNameByNumberSupport(const Phonebook* phonebook, const char* numberToFind)
+{
     Note* notes = phonebook->notes;
     int numberOfNotes = phonebook->numberOfNotes;
     for (int i = 0; i < numberOfNotes; ++i)
@@ -168,14 +181,27 @@ void findNameByNumber(const Phonebook* phonebook)
         Note note = notes[i];
         if (stringsAreEqual(note.number, numberToFind))
         {
-            printf("%s\n", note.name);
-            return;
+            return note.name;
         }
     }
-    printf("Number not found\n");
+    return NULL;
 }
 
-int saveNotesToFile(const Phonebook* phonebook, const char* nameOfFile)
+void findNameByNumber(const Phonebook* phonebook)
+{
+    char numberToFind[64] = { '\0' };
+    printf("Enter a number: ");
+    scanf_s("%s", numberToFind, 63);
+    char* foundName = findNameByNumberSupport(phonebook, numberToFind);
+    if (foundName == NULL)
+    {
+        printf("Number not found\n");
+        return;
+    }
+    printf("%s\n", foundName);
+}
+
+int saveNotesToFileSupport(const Phonebook* phonebook, const char* nameOfFile)
 {
     FILE* outputFile = NULL;
     int errorCode = fopen_s(&outputFile, nameOfFile, "w");
@@ -190,28 +216,97 @@ int saveNotesToFile(const Phonebook* phonebook, const char* nameOfFile)
         Note note = notes[i];
         fprintf(outputFile, "%s %s\n", note.name, note.number);
     }
-    printf("Notes saved\n");
     fclose(outputFile);
+    return 0;
+}
+
+int saveNotesToFile(const Phonebook* phonebook, const char* nameOfFile)
+{
+    int errorCode = saveNotesToFileSupport(phonebook, nameOfFile);
+    if (errorCode != 0)
+    {
+        return 1;
+    }
+    printf("Notes saved\n");
     return 0;
 }
 
 void freePhonebook(Phonebook* phonebook)
 {
     Note* notes = phonebook->notes;
-    int numberOfNotes = phonebook->numberOfNotes;
-    for (int i = 0; i < numberOfNotes; ++i)
+    int* numberOfNotes = &phonebook->numberOfNotes;
+    for (int i = 0; i < *numberOfNotes; ++i)
     {
         Note note = notes[i];
         free(note.name);
         free(note.number);
     }
     free(notes);
+    *numberOfNotes = 0;
 }
 
-bool testForReadIsPassed()
+char* stringMul(const char* string, const int multiplier)
+{
+    size_t lengthOfOriginalString = strlen(string);
+    char* newString = (char*)calloc(lengthOfOriginalString * multiplier + 1, sizeof(char));
+    if (newString == NULL)
+    {
+        return NULL;
+    }
+    int j = 0;
+    for (int i = 0; i < lengthOfOriginalString * multiplier; ++i)
+    {
+        newString[i] = string[j];
+        ++j;
+        if (j >= lengthOfOriginalString)
+        {
+            j = 0;
+        }
+    }
+    return newString;
+}
+
+bool testForAddIsPassed()
 {
     Phonebook phonebook = { .notes = NULL, .numberOfNotes = 0 };
     int errorCode = readNotesFromFile(&phonebook, TEST_FILE_1);
+    if (errorCode != 0)
+    {
+        return false;
+    }
+    int errorCode1 = addNoteSupport(&phonebook, "abababa", "99");
+    Note note1 = phonebook.notes[0];
+    bool testOneIsPassed = errorCode1 == 0 && stringsAreEqual(note1.name, "abababa")
+        && stringsAreEqual(note1.number, "99");
+
+    int errorCode2 = addNoteSupport(&phonebook, "", "");
+    Note note2 = phonebook.notes[1];
+    bool testTwoIsPassed = errorCode2 == 0 && stringsAreEqual(note2.name, "")
+        && stringsAreEqual(note2.number, "");
+
+    char* name3 = stringMul("abc", 21);
+    char* number3 = stringMul("8", 63);
+    if (name3 == NULL || number3 == NULL)
+    {
+        free(name3);
+        free(number3);
+        return false;
+    }
+    int errorCode3 = addNoteSupport(&phonebook, name3, number3);
+    Note note3 = phonebook.notes[2];
+    bool testThreeIsPassed = errorCode3 == 0 && stringsAreEqual(note3.name, name3)
+        && stringsAreEqual(note3.number, number3) && phonebook.numberOfNotes == 3;
+    free(name3);
+    free(number3);
+
+    freePhonebook(&phonebook);
+    return testOneIsPassed && testTwoIsPassed && testThreeIsPassed;
+}
+
+bool testForReadAndSaveIsPassed()
+{
+    Phonebook phonebook = { .notes = NULL, .numberOfNotes = 0 };
+    int errorCode = readNotesFromFile(&phonebook, TEST_FILE_2);
     if (errorCode != 0)
     {
         return false;
@@ -228,12 +323,47 @@ bool testForReadIsPassed()
     bool testThreeIsPassed = stringsAreEqual(note3.name, "pasha")
         && stringsAreEqual(note3.number, "8(999)1112233");
 
+    errorCode = saveNotesToFileSupport(&phonebook, TEST_FILE_2);
+    bool testFourIsPassed = errorCode == 0 && phonebook.numberOfNotes == 3;
+
+    freePhonebook(&phonebook);
     return testOneIsPassed && testTwoIsPassed && testThreeIsPassed && testFourIsPassed;
+}
+
+bool testForFindIsPassed()
+{
+    Phonebook phonebook = { .notes = NULL, .numberOfNotes = 0 };
+    int errorCode = readNotesFromFile(&phonebook, TEST_FILE_3);
+    if (errorCode != 0)
+    {
+        return false;
+    }
+    char* test1 = findNumberByNameSupport(&phonebook, "sasha");
+    bool testOneIsPassed = stringsAreEqual(test1, "89001112233");
+
+    char* test2 = findNameByNumberSupport(&phonebook, "World");
+    bool testTwoIsPassed = stringsAreEqual(test2, "Hello");
+
+    char* test3 = findNumberByNameSupport(&phonebook, "17");
+    bool testThreeIsPassed = stringsAreEqual(test3, "03");
+
+    char* test4 = findNameByNumberSupport(&phonebook, "babab");
+    bool testFourIsPassed = stringsAreEqual(test4, "ababa");
+
+    char* test5 = findNumberByNameSupport(&phonebook, "Matmex");
+    bool testFiveIsPassed = test5 == NULL && phonebook.numberOfNotes == 100;
+
+    char* test6 = findNameByNumberSupport(&phonebook, "123456789");
+    bool testSixIsPassed = test6 == NULL;
+
+    freePhonebook(&phonebook);
+    return testOneIsPassed && testTwoIsPassed && testThreeIsPassed
+        && testFourIsPassed && testFiveIsPassed && testSixIsPassed;
 }
 
 bool allTestsArePassed()
 {
-    return testForReadPassed() && testForAddIsPassed();
+    return testForAddIsPassed() && testForReadAndSaveIsPassed() && testForFindIsPassed();
 }
 
 int main()
@@ -277,7 +407,7 @@ int main()
             errorCode = saveNotesToFile(&phonebook, NAME_OF_FILE);
             break;
         default:
-            printf("Unknown command");
+            printf("Unknown command\n");
         }
         if (errorCode != 0)
         {
