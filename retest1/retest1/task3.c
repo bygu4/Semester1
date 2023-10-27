caee4a3dd72bfab8d970352d93d25be969856fa5
Alexander <sacha05bugaev@gmail.com>
2023-10-27 15:11:54 +0300

    добавить тесты

--- retest1/retest1/task3.c
+++ retest1/retest1/task3.c
@@ -3,7 +3,10 @@
 #include <stdbool.h>
 
 #define SUCCESS 0
+#define TEST_FAILED -1
 #define BAD_ALLOCATION 1
+
+#define NUMBER_OF_CHARACTERS 256
 
 #define NAME_OF_FILE "task3.txt"
 #define TEST_FILE "test.txt"
@@ -13,7 +16,7 @@ void readStringAndCount(FILE* const file, int* const characters)
     char character = fgetc(file);
     while (character != '\n' && !feof(file))
     {
-        ++characters[(size_t)character + 128];
+        ++characters[(size_t)character + NUMBER_OF_CHARACTERS / 2];
         character = fgetc(file);
     }
 }
@@ -26,7 +29,7 @@ int* countNumberOfEntriesForEveryCharacter(const char* const nameOfFile, int* co
     {
         return NULL;
     }
-    int* characters = (int*)calloc(256, sizeof(int));
+    int* characters = (int*)calloc(NUMBER_OF_CHARACTERS, sizeof(int));
     if (characters == NULL)
     {
         *errorCode = BAD_ALLOCATION;
@@ -43,19 +46,71 @@ int* countNumberOfEntriesForEveryCharacter(const char* const nameOfFile, int* co
 
 void printResult(const int* const result)
 {
-    for (size_t i = 0; i < 256; ++i)
+    for (size_t i = 0; i < NUMBER_OF_CHARACTERS; ++i)
     {
-        char character = (char)i - 128;
+        char character = (char)i - NUMBER_OF_CHARACTERS / 2;
         int numberOfEntries = result[i];
         if (numberOfEntries != 0)
         {
-            printf("%c: %d\n", character, numberOfEntries);
+            if (character == ' ')
+            {
+                printf("Space: %d\n", numberOfEntries);
+            }
+            else
+            {
+                printf("%c: %d\n", character, numberOfEntries);
+            }
         }
+    }
+}
+
+bool arraysAreEqual(const int* const array1, const int* const array2, const size_t sizeOfArray)
+{
+    for (size_t i = 0; i < sizeOfArray; ++i)
+    {
+        if (array1[i] != array2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testCase(const char* const nameOfFile, int* expectedArray)
+{
+    int errorCode = -1;
+    int* result = countNumberOfEntriesForEveryCharacter(nameOfFile, &errorCode);
+    bool testIsPassed = errorCode == SUCCESS && arraysAreEqual(result, expectedArray, NUMBER_OF_CHARACTERS);
+    free(result);
+    free(expectedArray);
+    return testIsPassed;
+}
+
+bool test(void)
+{
+    int* expectedArray = (int*)calloc(NUMBER_OF_CHARACTERS, sizeof(int));
+    expectedArray[(size_t)'8' + NUMBER_OF_CHARACTERS / 2] = 8;
+    expectedArray[(size_t)'7' + NUMBER_OF_CHARACTERS / 2] = 7;
+    expectedArray[(size_t)'a' + NUMBER_OF_CHARACTERS / 2] = 2;
+    expectedArray[(size_t)'b' + NUMBER_OF_CHARACTERS / 2] = 3;
+    expectedArray[(size_t)'c' + NUMBER_OF_CHARACTERS / 2] = 1;
+    expectedArray[(size_t)' ' + NUMBER_OF_CHARACTERS / 2] = 2;
+    expectedArray[(size_t)'#' + NUMBER_OF_CHARACTERS / 2] = 2;
+    bool testOneIsPassed = testCase(TEST_FILE, expectedArray);
+    if (!testOneIsPassed)
+    {
+        printf("Test 1 has failed\n");
+        return false;
     }
 }
 
 int main(void)
 {
+    const bool allTestsArePassed = test();
+    if (!allTestsArePassed)
+    {
+        return TEST_FAILED;
+    }
     int errorCode = -1;
     int* result = countNumberOfEntriesForEveryCharacter(NAME_OF_FILE, &errorCode);
     if (errorCode != SUCCESS)
