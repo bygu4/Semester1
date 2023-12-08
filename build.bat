849b0865637614357db79937bd4c0164065ac6b8
Alexander <sacha05bugaev@gmail.com>
2023-12-08 21:18:15 +0300

    добавить описание сборки

--- build.bat
+++ build.bat
@@ -5,11 +5,11 @@ for /r %%i in (*.vcxproj) do (
     if exist %%~dpi/Debug/%%~ni.exe (
         %%~dpi/Debug/%%~ni.exe -test
         if errorlevel 0 (
-            echo %%~ni: test passed
+            echo - %%~ni: test passed
         ) else (
-            echo %%~ni: test failed
+            echo - %%~ni: test failed
         )
     ) else (
-        echo %%~ni: build failed
+        echo - %%~ni: build failed
     )
 )
