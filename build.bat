a56018e4a93157e713a3373f9e86b16cadd8c97c
Alexander <sacha05bugaev@gmail.com>
2023-12-08 23:22:27 +0300

    исправить скрипт

--- build.bat
+++ build.bat
@@ -1,14 +1,18 @@
 @echo off
 
+set root=%cd%
+
 for /r %%i in (*.vcxproj) do (
     msbuild %%i -nologo -noConsoleLogger
-    if exist %%~dpi/Debug/%%~ni.exe (
-        %%~dpi/Debug/%%~ni.exe -test
+    if exist %%~dpi\Debug\%%~ni.exe (
+        cd %%~dpi
+        %%~dpi\Debug\%%~ni.exe -test
         if errorlevel 0 (
             echo - %%~ni: test passed
         ) else (
             echo - %%~ni: test failed
         )
+        cd %root%
     ) else (
         echo - %%~ni: build failed
     )
--- week4/modulesAndFiles/modulesAndFiles.c
+++ week4/modulesAndFiles/modulesAndFiles.c
@@ -2,10 +2,8 @@
 #include "quicksort.h"
 #include "findMostCommonElement.h"
 #include "test.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdlib.h"
 #include <stdbool.h>
-#include <string.h>
 
 #define NAME_OF_FILE "modulesAndFiles.txt"
 #define TEST_FAILED -1
